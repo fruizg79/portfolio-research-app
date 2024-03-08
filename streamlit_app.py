8bbbd8ce79fdf63980ee92217a73f8611f287e00
Fernando Ruiz <fruizg79@gmail.com>
2024-03-08 19:11:52 +0100

    use of Black Litterman
    Co-Authored-By: Fernando Ruiz <fruizg79@gmail.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,40 @@
 import altair as alt
+import math
 import numpy as np
+import os
 import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
-import streamlit as st
-import pandas as pd
-import pandas as pd
+import numpy as np
+import cblacklitterman
 
 
+p_matrix = np.array([[ 1.,  0.,  0.,  0.,  0.,  0.,  0.],
+       [ 0.,  1.,  0.,  0.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  1., -1.,  0.,  0.],
+       [ 0.,  0., -1.,  1.,  0.,  0.,  0.],
+       [ 0.,  0.,  0.,  0.,  0.,  1.,  0.],
+       [ 0.,  0.,  0.,  1.,  0.,  0.,  0.]])
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
 
-    chart_data
+nu_vector = np.array([-0.25,0.00,0.25,0.25,-0.50,0.25])
+reco_vector =['sell','neutral','buy','buy','strong_sell','buy']
 
+c_coef  = 1
 
+tau = 0.40
+risk_free_rate = 0.0020
+conf_level = np.array([0.5,0.5,0.5,0.5,0.5,0.5])
+lambda_param = 7
+port_wts_eq =np.array([0.200,0.330,0.050,0.200,0.150,0.020,0.050])
+
+bl = cblacklitterman.CBlack_litterman(risk_free_rate,tau,
+                sigma,p_matrix,lambda_param,
+                port_wts_eq, conf_level,c_coef, reco_vector)
+
+bl.get_eq_risk_premium()
+eq_ret = bl.get_eq_returns()
+bl.get_view_returns()
+bl.get_omega()
+bl_ret = bl.get_bl_returns()
 
 
